69ae7fe0292ad19b377133aca6675a48e4929fb4
Miarotianastephan <miarotianastephan01@gmail.com>
2023-04-03 21:41:40 +0300

    to_discard

--- script.bat
+++ script.bat
@@ -5,5 +5,8 @@ javac -d framework\src framework\src\*.java
 @REM create jar 
 jar cf test_framework\src\WEB-INF\lib\framework_jar.jar framework\src\etu1846
 
+@REM compile the lib --into classes
+@REM javac --classpath test_framework\src\WEB-INF\lib\framework_jar.jar -d test_framework\src\WEB-INF\classes 
+
 @REM transform to war 
 jar -cvf myapp.war -C test_framework\src .
